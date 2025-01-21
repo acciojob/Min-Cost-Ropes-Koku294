544eab89b34ff0d9f3bf6aad06d9e9887d64dad1
Koku294 <90713556+Koku294@users.noreply.github.com>
2025-01-21 22:42:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,35 @@
-function mincost(arr)
-{ 
-//write your code here
-// return the min cost
-  
+function mincost(arr) {
+   
+    if (arr.length === 1) return 0;
+
+   
+    const MinHeap = require('heap');
+    const heap = new MinHeap((a, b) => a - b);
+
+   
+    for (let length of arr) {
+        heap.push(length);
+    }
+
+    let totalCost = 0;
+
+    
+    while (heap.size() > 1) {
+        
+        let first = heap.pop();
+        let second = heap.pop();
+
+        /
+        let cost = first + second;
+
+        
+        totalCost += cost;
+
+        
+        heap.push(cost);
+    }
+
+    return totalCost;
 }
 
-module.exports=mincost;
+module.exports = mincost;
